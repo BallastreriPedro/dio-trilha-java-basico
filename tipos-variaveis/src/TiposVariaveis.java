d09522a81a8176a17b501d8aee850e98d6ac5eef
Pedro Ballastreri <gballastreri@gmail.com>
2025-02-17 15:32:09 -0300

    adição dos tipos e variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,10 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
+        String nome = "Pedro"; // Tipo para palavras
+        int idade = 22; // Tipo para numeros inteiros
+        double peso = 68.5; //Tipo para numeros com ponto 
+        char sexo = 'M'; //Tipo para apenas um caractere
+        boolean doadorOrgao = false; //Tipo para verdadeiro ou falso
         
     }
 }
