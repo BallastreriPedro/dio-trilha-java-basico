d3ea7907524ad34423b429a61cbbe7f107bada32
Pedro Ballastreri <gballastreri@gmail.com>
2025-02-17 15:59:03 -0300

    adição do operador ternario

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -6,5 +6,10 @@ public class TiposVariaveis {
         char sexo = 'M'; //Tipo para apenas um caractere
         boolean doadorOrgao = false; //Tipo para verdadeiro ou falso
         
+
+        int a = 6;
+        int b = 6;
+        int resultado = a == b ? 1 : 0;
+        System.out.print(resultado); // operador ternario, verifica se a condição é verdadeira ou falsa. Se verdadeira, retorna o valor após o "?", caso contrario, retornara o valor após o ":".
     }
 }
