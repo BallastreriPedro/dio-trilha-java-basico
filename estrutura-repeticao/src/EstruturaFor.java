2aa6354740f9bc86b77b32961c71a568e7a42fff
Pedro Ballastreri <gballastreri@gmail.com>
2025-02-19 14:08:19 -0300

    adição das estrutura de repetições

---
+++ estrutura-repeticao/src/EstruturaFor.java
@@ -0,0 +1,8 @@
+public class EstruturaFor {
+    public static void main(String[] args) throws Exception {
+
+        for(int carneirinho = 1; carneirinho <= 20; carneirinho ++){
+            System.out.println("Contando carneirinhos: " + carneirinho);
+        }
+    }
+}
